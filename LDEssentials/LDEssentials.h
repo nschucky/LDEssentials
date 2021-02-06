d867138fc45e8b3906b2b01395b7cc8034269fdd
nschucky <antonioalvesdev@gmail.com>
2021-02-05 23:50:44 -0800

    Initial Commit

---
+++ LDEssentials/LDEssentials.h
@@ -0,0 +1,18 @@
+//
+//  LDEssentials.h
+//  LDEssentials
+//
+//  Created by Antonio Alves on 2/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LDEssentials.
+FOUNDATION_EXPORT double LDEssentialsVersionNumber;
+
+//! Project version string for LDEssentials.
+FOUNDATION_EXPORT const unsigned char LDEssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDEssentials/PublicHeader.h>
+
+
