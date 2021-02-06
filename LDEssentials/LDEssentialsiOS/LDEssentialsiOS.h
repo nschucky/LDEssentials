d683b953ff3185371c745d99e61d20840f1405dd
nschucky <antonioalvesdev@gmail.com>
2021-02-06 11:41:13 -0800

    Add LDEssentialsiOS framework (prod and test) targets for iOS platform-specific components

---
+++ LDEssentials/LDEssentialsiOS/LDEssentialsiOS.h
@@ -0,0 +1,18 @@
+//
+//  LDEssentialsiOS.h
+//  LDEssentialsiOS
+//
+//  Created by Antonio Alves on 2/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LDEssentialsiOS.
+FOUNDATION_EXPORT double LDEssentialsiOSVersionNumber;
+
+//! Project version string for LDEssentialsiOS.
+FOUNDATION_EXPORT const unsigned char LDEssentialsiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDEssentialsiOS/PublicHeader.h>
+
+
