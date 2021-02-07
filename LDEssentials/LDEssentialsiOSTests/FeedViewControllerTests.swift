a462e539c0e1fcc347037936c4e76e3543de194d
nschucky <antonioalvesdev@gmail.com>
2021-02-06 22:46:35 -0800

    Extract pull to refresh simulation into a reusable extension on UIRefreshControl

--- LDEssentials/LDEssentialsiOSTests/FeedViewControllerTests.swift
+++ LDEssentials/LDEssentialsiOSTests/FeedViewControllerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import UIKit
 import LDEssentials
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
     
     var loader: FeedLoader?
     
@@ -20,6 +20,13 @@ final class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
         loader?.load { _ in }
     }
     
@@ -40,6 +47,17 @@ class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadAllCount, 1)
     }
     
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadAllCount, 2)
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadAllCount, 3)
+    }
+    
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
         let sut = FeedViewController(loader: loader)
@@ -58,3 +76,10 @@ class FeedViewControllerTests: XCTestCase {
     }
 }
 
+extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { (target as NSObject).perform(Selector($0)) }
+        }
+    }
+}
