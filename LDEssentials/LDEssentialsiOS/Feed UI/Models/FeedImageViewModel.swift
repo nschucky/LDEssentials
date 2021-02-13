5b16a46fcdcc8e5641d9e98b7848e2b5e9f42daf
nschucky <antonioalvesdev@gmail.com>
2021-02-13 12:48:38 -0800

    Decouple `FeedImageViewModel` from `UIKit` by creating a transformation closure that converts an image `Data` value into a generic `Image` type. When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`.

--- LDEssentials/LDEssentialsiOS/Feed UI/Models/FeedImageViewModel.swift
+++ LDEssentials/LDEssentialsiOS/Feed UI/Models/FeedImageViewModel.swift
@@ -9,15 +9,16 @@ import Foundation
 import LDEssentials
 import UIKit
 
-final class FeedImageViewModel {
+final class FeedImageViewModel<Image> {
     
     typealias Observer<T> = (T) -> Void
     
     private var task: FeedImageDataLoaderTask?
     private let model: FeedImage
     private let imageLoader: FeedImageDataLoader
+    private let imageTransformer: (Data) -> Image?
     
-    var onImageLoad: Observer<UIImage>?
+    var onImageLoad: Observer<Image>?
     var onImageLoadingStateChange: Observer<Bool>?
     var onShouldRetryImageLoadStateChange: Observer<Bool>?
     
@@ -33,9 +34,10 @@ final class FeedImageViewModel {
         return location != nil
     }
     
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
         self.model = model
         self.imageLoader = imageLoader
+        self.imageTransformer = imageTransformer
     }
     
     func loadImageData() {
@@ -47,7 +49,7 @@ final class FeedImageViewModel {
     }
     
     private func handle(_ result: FeedImageDataLoader.Result) {
-        if let image = (try? result.get()).flatMap(UIImage.init) {
+        if let image = (try? result.get()).flatMap(imageTransformer) {
             onImageLoad?(image)
         } else {
             onShouldRetryImageLoadStateChange?(true)
