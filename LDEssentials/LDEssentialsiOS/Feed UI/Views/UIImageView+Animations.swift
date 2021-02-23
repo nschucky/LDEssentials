8526d7c13434a5f4d9d6c131e69e8eaa6abe8c40
nschucky <antonioalvesdev@gmail.com>
2021-02-23 12:59:05 -0800

    Display image with animation

---
+++ LDEssentials/LDEssentialsiOS/Feed UI/Views/UIImageView+Animations.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Animations.swift
+//  LDEssentialsiOS
+//
+//  Created by Antonio Alves on 2/23/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil  else { return }
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
+
