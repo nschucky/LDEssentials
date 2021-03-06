3e6100761e52bba890f16335b17c43e2dfce26f8
nschucky <antonioalvesdev@gmail.com>
2021-03-05 16:32:52 -0800

    `FeedPresenter` displays no error on `didStartLoadingFeed`

--- LDEssentials/LDEssentialsTests/Feed Presentation/FeedPresenterTests.swift
+++ LDEssentials/LDEssentialsTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,28 @@
 
 import XCTest
 
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+struct FeedErrorViewModel {
+     let message: String?
+
+     static var noError: FeedErrorViewModel {
+         return FeedErrorViewModel(message: nil)
+     }
+}
+
 final class FeedPresenter {
-    init(view: Any) {
-        
+    
+    let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
     }
 }
 
@@ -20,18 +39,34 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        sut.didStartLoadingFeed()
+        XCTAssertEqual(view.messages, [.display(errorMessage: nil)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         trackForMemoryLeaks(view)
         trackForMemoryLeaks(sut)
         return (sut, view)
     }
     
-    private class ViewSpy {
-        let messages: [Any] = []
+    private class ViewSpy: FeedErrorView {
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: nil))
+        }
+        
+        
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        var messages: [Message] = []
     }
     
 }
